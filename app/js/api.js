e16501309ae737439786c72bf2913a71657bde88
evelynzhang88 <1054982653@qq.com>
2016-04-20 20:10:36 +0800

    api

--- app/js/api.js
+++ app/js/api.js
@@ -1,6 +1,17 @@
 /*All the api collection*/
 API = {
     //{"status":1,"msg":{"id":"1","openid":"oKCDxjivJ92ky4dxLT8dt1jcXtn4","nickname":"nickname","headimgurl":"headimgurl","greeting":false,"background":false,"ballot":"1”}}
+    //api/status
+    isFollow:function(callback){
+        $.ajax({
+            url:'/api/status',
+            type:'POST',
+            dataType:'json',
+            success:function(data){
+                return callback(data);
+            }
+        });
+    },
     isLogin:function(callback){
         $.ajax({
             url:'/api/islogin',
@@ -76,32 +87,36 @@ API = {
 
 //test api
 (function(){
-    //API.writeGreeting({greeting:'happy',background:1},function(data){
+    //API.isFollow(function(data){
     //    console.log(data);
     //});
 
-    //API.ballot({id:121212},function(data){
+    //API.writeGreeting({greeting:'happy',background:2},function(data){
     //    console.log(data);
     //});
 
-    //API.sendVerifycode({mobile:18502130583},function(data){
+    //API.ballot({id:'123'},function(data){
     //    console.log(data);
     //});
 
-    //API.customerBind({mobile:18502130583,verifycode:'8288'},function(data){
+    //API.sendVerifycode({mobile:'18502130583'},function(data){
     //    console.log(data);
     //});
 
-    API.customerRegister({
-        firstname:'zhang',
-        lastname:'ya',
-        mobile :'18502130583',
-        email:'1@qq.com',
-        gender:'女',
-        openid:'123',
-    },
-    function(data){
-        console.log(data);
-    });
+    //API.customerBind({mobile:'18502130583',verifycode:'76j8'},function(data){
+    //    console.log(data);
+    //});
+
+    //API.customerRegister({
+    //    firstname:'zhang',
+    //    lastname:'ya',
+    //    mobile :'18502130583',
+    //    email:'1@qq.com',
+    //    gender:'女',
+    //    openid:'123',
+    //},
+    //function(data){
+    //    console.log(data);
+    //});
 
 })();
