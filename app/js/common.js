cc5283926194a2b14213ded6228120c99f8b867e
evelynzhang88 <1054982653@qq.com>
2016-04-25 21:32:58 +0800

    fresh

--- app/js/common.js
+++ app/js/common.js
@@ -30,6 +30,24 @@ function gotoPin(i) {
 		isWx:function(){
 			return (/micromessenger/.test(ua)) ? true:false;
 		},
+		queryString:function(name){
+			//var curUrl = window.location.search;
+			var name,value,i;
+			var str=location.href;
+			var num=str.indexOf("?")
+			str=str.substr(num+1);
+			var arrtmp=str.split("&");
+			for(i=0;i < arrtmp.length;i++) {
+				num=arrtmp[i].indexOf("=");
+				if(num>0)
+				{
+					name=arrtmp[i].substring(0,num);
+					value=arrtmp[i].substr(num+1);
+					return value;
+				}
+			}
+
+		},
 		msgBox:{
 			add:function(msg,long){
 				if(long){
--- app/js/photopage.js
+++ app/js/photopage.js
@@ -5,21 +5,30 @@
 $(document).ready(function(){
     //var enableScroll = true;
     console.log('photo page');
-    //Api.galleryList({
-    //},function(data){
-    //    //enableScroll = true;
-    //    if(data.status ==1){
-    //        var listData = data.msg.list;
-    //        var listHtml = '';
-    //    }else{
-    //        alert(data.msg);
-    //    }
-    //
-    //});
+    var qsid = Common.queryString('id'),
+        imgid;
+    Api.getGreeting({
+        id:qsid
+    },function(data){
+        //enableScroll = true;
+        if(data.status ==1){
+            var data = data.msg;
+            var Html = '';
+            $('.photo-frame').attr('class','photo-frame photo photo-'+data.background);
+            $('.p-inner img').attr('src',data.image);
+            $('.leave-words').html(data.greeting);
+            $('.user-name').html(data.nickname);
+            $('.icon-good').html(data.ballot);
+            imgid = data.id;
+        }else{
+            alert(data.msg);
+        }
+
+    });
 
     $('.btn-like').on('click', function(){
         Api.ballot({
-            id:''
+            id:imgid
         },function(data){
             console.log(data);
         });
