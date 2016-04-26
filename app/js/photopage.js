d8c0d1cd57f9fa9123a6da0799b223da83b7cddb
evelynzhang88 <1054982653@qq.com>
2016-04-26 11:52:48 +0800

    click page

--- app/js/photopage.js
+++ app/js/photopage.js
@@ -3,34 +3,57 @@
  * Just for photo page
  * */
 $(document).ready(function(){
-    //var enableScroll = true;
-    console.log('photo page');
     var qsid = Common.queryString('id'),
-        imgid;
-    Api.getGreeting({
-        id:qsid
-    },function(data){
-        //enableScroll = true;
-        if(data.status ==1){
-            var data = data.msg;
-            var Html = '';
-            $('.photo-frame').attr('class','photo-frame photo photo-'+data.background);
-            $('.p-inner img').attr('src',data.image);
-            $('.leave-words').html(data.greeting);
-            $('.user-name').html(data.nickname);
-            $('.icon-good').html(data.ballot);
-            imgid = data.id;
-        }else{
-            alert(data.msg);
-        }
+        uid;
+    Api.isLogin(function(data){
+        if(data.status==1){
 
+            //loading the page content
+            Api.getGreeting({
+                id:qsid
+            },function(data){
+                //enableScroll = true;
+                if(data.status ==1){
+                    var data = data.msg;
+                    var Html = '';
+                    $('.photo-frame').attr('class','photo-frame photo photo-'+data.background);
+                    $('.p-inner img').attr('src',data.image);
+                    $('.leave-words').html(data.greeting);
+                    $('.user-name').html(data.nickname);
+                    $('.icon-good').html(data.ballot);
+                    uid = data.uid;
+                }else{
+                    //alert(data.msg);
+                }
+
+            });
+            //    logged
+            if(data.msg.background){
+                $('.btn-sprite').eq(0).removeClass('btn-joinplay').addClass('btn-gogallery');
+            }else{
+                //not submit your image
+                $('.btn-sprite').eq(0).addClass('btn-joinplay').removeClass('btn-gogallery');
+            }
+        }else{
+            //alert(data.msg);
+        }
     });
 
+    //like
+    var enabled = true;
     $('.btn-like').on('click', function(){
+        if(!enabled) return;
+        enabled = false;
         Api.ballot({
-            id:imgid
+            id:uid
         },function(data){
-            console.log(data);
+            enabled = true;
+            if(data.status==1){
+                var voteNum = parseInt($('.icon-good').html());
+                $('.icon-good').html(voteNum++);
+            }else{
+                alert(data.msg);
+            }
         });
     });
 
