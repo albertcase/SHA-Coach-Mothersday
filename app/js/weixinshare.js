b084e900a4af2cc50f9e0f0d26d7fb68e4eb1f85
evelynzhang88 <1054982653@qq.com>
2016-04-27 13:02:17 +0800

    weixin share

--- app/js/weixinshare.js
+++ app/js/weixinshare.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
         });
         wx.onMenuShareAppMessage({
             title: '大声说出对妈妈的爱，赢取全新Saddle手袋！',
-            desc: '大声说出对妈妈的爱，赢取全新Saddle手袋！',
+            desc: 'COACH致爱母亲节',
             link: sharepath,
             imgUrl: window.location.origin+'/app/images/kv.png',
             type: '',
