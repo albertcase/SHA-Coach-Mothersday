9dd96de4a45c8afde73223955bd1c378823695cc
evelynzhang88 <1054982653@qq.com>
2016-04-29 14:17:24 +0800

    no message

--- app/js/gallery.js
+++ app/js/gallery.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 			var listHtml = '';
 			totalPage = parseInt(data.msg.totalpage);
 			for(var i=0;i<listData.length;i++){
-				listHtml = listHtml+'<li class="item">'+
+				listHtml += listHtml+'<li class="item">'+
 					'<div class="top-banner">'+
 					'<span class="user-name">'+listData[i].nickname+'</span>'+
 					'<span class="icon-good">'+listData[i].ballot+'</span>'+
@@ -61,7 +61,7 @@ $(document).ready(function(){
 					var listData = data.msg.list;
 					var listHtml = '';
 					for(var i=0;i<listData.length;i++){
-						listHtml = '<li class="item">'+
+						listHtml += '<li class="item">'+
 							'<div class="top-banner">'+
 							'<span class="user-name">'+listData[i].nickname+'</span>'+
 							'<span class="icon-good">'+listData[i].ballot+'</span>'+
